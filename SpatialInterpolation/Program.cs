703e41267bcfe127d46e864925cdc6dc0efbb9eb
ianporpino1 <38231124+ianporpino1@users.noreply.github.com>
2024-03-11 14:49:13 -0300

    Add files via upload

---
+++ SpatialInterpolation/Program.cs
@@ -0,0 +1,71 @@
+﻿
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        string fileKnownPoints = "data/data.csv";
+        List<double> x_known = new List<double>();
+        List<double> y_known = new List<double>();
+        List<double> z_known = new List<double>();
+        DateTime startTime = DateTime.Now;
+
+        try
+        {
+            using (StreamReader sr = new StreamReader(fileKnownPoints))
+            {
+                sr.ReadLine();
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    x_known.Add(double.Parse(parts[0]));
+                    y_known.Add(double.Parse(parts[1]));
+                    z_known.Add(double.Parse(parts[2]));
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Error reading the file: " + e.Message);
+        }
+
+        string fileUnknownPoints = "src/data/unknown_points.csv";
+        List<double> x_unknown = new List<double>();
+        List<double> y_unknown = new List<double>();
+
+        try
+        {
+            using (StreamReader sr = new StreamReader(fileUnknownPoints))
+            {
+                sr.ReadLine();
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    x_unknown.Add(double.Parse(parts[0]));
+                    y_unknown.Add(double.Parse(parts[1]));
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Error reading the file: " + e.Message);
+        }
+
+      
+
+        List<double> z_interpolated = SpatialInterpolation.InverseDistanceWeighting(x_known, y_known, z_known, x_unknown, y_unknown, 2.0);
+
+        DateTime endTime = DateTime.Now;
+        TimeSpan duration = endTime - startTime;
+
+        Console.WriteLine("Tempo de execução: " + duration.TotalSeconds + " segundos");
+
+        foreach (double val in z_interpolated)
+        {
+            Console.WriteLine(val);
+        }
+    }
+}
+
---
+++ SpatialInterpolation/SpatialInterpolation.cs
@@ -0,0 +1,25 @@
+
+using System.Collections;
+
+public class SpatialInterpolation
+{
+    public static List<double> InverseDistanceWeighting(List<double> x_known, List<double> y_known, List<double> z_known, List<double> x_unknown, List<double> y_unknown, double power)
+    {
+        List<double> z_unknown = new List<double>(x_unknown.Count);
+        for (int i = 0; i < x_unknown.Count; i++)
+        {
+            double sumWeights = 0.0;
+            double sumWeightedValues = 0.0;
+            for (int j = 0; j < x_known.Count; j++)
+            {
+                double distance = Math.Sqrt(Math.Pow(x_known[j] - x_unknown[i], 2) + Math.Pow(y_known[j] - y_unknown[i], 2));
+                double weight = 1.0 / Math.Pow(distance, power);
+                sumWeights += weight;
+                sumWeightedValues += weight * z_known[j];
+            }
+            z_unknown[i] = sumWeightedValues / sumWeights;
+        }
+        return z_unknown;
+    }
+}
+
