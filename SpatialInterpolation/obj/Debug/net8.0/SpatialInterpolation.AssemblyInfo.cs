8356436ef9e9a6e6c27ac2e76c1bdaedbeb5abaa
ianporpino1 <ianporpino127@gmail.com>
2024-05-22 12:30:42 -0300

    updated

--- SpatialInterpolation/obj/Debug/net8.0/SpatialInterpolation.AssemblyInfo.cs
+++ SpatialInterpolation/obj/Debug/net8.0/SpatialInterpolation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpatialInterpolation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d667dd1dda71a49d58e971b66832aa55091a404")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a234d11d4e84029d69ce4b9d30b19ae8f8532f1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpatialInterpolation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpatialInterpolation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
