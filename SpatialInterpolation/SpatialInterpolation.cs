c343b9808212fe48a337b98efc2b9321e3c71856
ianporpino1 <ianporpino127@gmail.com>
2024-05-22 18:52:30 -0300

    updated

--- SpatialInterpolation/SpatialInterpolation.cs
+++ SpatialInterpolation/SpatialInterpolation.cs
@@ -12,13 +12,32 @@ public class SpatialInterpolation
 
             foreach (var k in knownPoints)
             {
-                double distance = Math.Sqrt(Math.Pow(k.X - u.X, 2) + Math.Pow(k.Y - u.Y, 2));
-                double weight = 1 / Math.Pow(distance, power);
+                double distance = Math.Sqrt( ((k.X - u.X) * (k.X - u.X) ) + ((k.Y - u.Y) * (k.Y - u.Y)) );
+                double weight = 1.0 / Pow(distance, power);
                 sumWeights += weight;
                 sumWeightedValues += weight * k.Z;
             }
             u.Z = sumWeightedValues / sumWeights;
             return u;
         };
+    }
+    
+    
+    
+    private static double Pow(double baseNumber, double exponent)
+    {
+        double result = 1;
+
+        for (int i = 0; i < Math.Abs(exponent); i++)
+        {
+            result *= baseNumber;
+        }
+
+        if (exponent < 0)
+        {
+            result = 1 / result;
+        }
+
+        return result;
     }
 }
